bb68844ec24927cd4a41692a5c4fc8ea94f71593
Piotr Suchy <piotrsuchy@proton.me>
2024-12-20 22:28:31 +0100

    Initial website build

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./layouts/**/*.html",
+    "./content/**/*.md",
+    "./content/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
